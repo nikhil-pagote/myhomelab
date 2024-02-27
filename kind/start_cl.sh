e3dcf87773eecb79e20e9a0095951174a48f14d2
Nikhil Pagote <nikhil.pagote@gmail.com>
2024-02-27 09:45:09 +0000

    updated code

--- kind/start_cl.sh
+++ kind/start_cl.sh
@@ -1,6 +1,9 @@
 # setup the cluster
 kind delete cluster --name=cl01
 kind create cluster --config=config.yml --name=cl01
+sleep(10)
+kubectl cluster-info --context kind-cl01
+
 
 # Setup prometheus monitoring
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
@@ -8,6 +11,9 @@ helm repo update
 #helm install [RELEASE_NAME] prometheus-community/kube-prometheus-stack
 helm install prometheus prometheus-community/kube-prometheus-stack
 #helm uninstall [RELEASE_NAME]
-
+sleep(30)
+kubectl --namespace default get pods -l "release=prometheus"
+helm show values prometheus-community/kube-prometheus-stack > promval.yaml
+# helm install prometheus prometheus-community/kube-prometheus-stack -f promval.yaml
 
 # Setup Grafana dashboard
